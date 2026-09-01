3b95d8b1b10d2b4991578fbf56bdab0f6992a6bc
Xaaxie <alamalan5678@gmail.com>
2026-09-01 19:17:30 +0700

    build: add Electron Forge packaging configuration

---
+++ forge.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  packagerConfig: {
+    asar: true
+  },
+  makers: [
+    { name: '@electron-forge/maker-squirrel', config: {} },
+    { name: '@electron-forge/maker-zip', platforms: ['darwin', 'linux', 'win32'] }
+  ]
+};
